349ce9feab846bd1a4c67094180a778386885514
crystal cardona <crystalcardona@pursuit.org>
2020-07-06 23:19:56 -0400

    db working

--- backend/db/schema.sql
+++ backend/db/schema.sql
@@ -20,18 +20,30 @@ CREATE TABLE users(
 
 CREATE TABLE posts(
     id SERIAL PRIMARY KEY,
-    post_id INT REFERENCES posts(id),
+    post_id INT REFERENCES users(id),
+    pictures VARCHAR,
+    captions VARCHAR,
     hashtag VARCHAR 
 );
 
 CREATE TABLE comments(
     id SERIAL PRIMARY KEY, 
-    comments_id INT REFERENCES post(id),
+    comments_id INT REFERENCES posts(id),
     comment VARCHAR
 );
 
 CREATE TABLE hashtags(
     id SERIAL PRIMARY KEY, 
-    post_id INT REFERENCES post(id),
+    post_id INT REFERENCES posts(id),
     hashtag VARCHAR
 );
+
+INSERT INTO users( firstName, lastName, userName, password,  email, user_pic)
+VALUES( 'CeeCee', 'Bliss', 'ceebliss', 'pursuit','cc@pursuit.org', 'https://pbs.twimg.com/profile_images/841015882797199361/Wigbpdj4_400x400.jpg');
+
+INSERT INTO posts (post_id, pictures, captions, hashtag)
+VALUES( 1, 'https://assets3.thrillist.com/v1/image/2791598/size/tl-full_width_tall_mobile.jpg', 'what a great time here', null);
+
+
+INSERT INTO hashtags(post_id, hashtag)
+VALUES( 1, '#summer19');
